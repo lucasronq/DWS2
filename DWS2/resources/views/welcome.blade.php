37a2069ee4f8f39b4947817eb0fb8cf1d967ba9d
lucasronq <73199582+lucasronq@users.noreply.github.com>
2023-05-02 05:41:00 -0900

    att
    att

---
+++ DWS2/resources/views/welcome.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.main')
+
+@section('title', "HDC Events")
+
+@section('content')
+
+@endsection
+<h1>Titulo</h1>
+<img src="/img/banner.jpg" alt="Banner">
+@if(10 > 5)
+    <p>A condição é true</p>
+@endif
+
+<p>{{ $nome }}</p>
+
+@if($nome == "Lucas")
+<p>O nome é Lucas</p>
+@elseif($nome == "Matheus")
+<p>O nome é {{$nome}} e ele tem {{$idade}} anos, e trabalho como {{$profissao}}.</p>
+@else
+<p>O nome não é Lucas</p>
+@endif
+
+@for($i = 0; $i < count($arr); $i++)
+    <p>{{$arr[$i] }} - {{$i}}</p>
+    @if($i == 2)
+    <p>o i é 2</p>
+    @endif
+@endfor
+
+@foreach ($nomes as $nome)
+    <p>{{$loop->index}}</p>
+    <p>{{$nome}}</p>    
+@endforeach
+
+@php
+    $name = "João";
+    echo $name;
+@endphp
+
+<!-- Coment HTML -->
+{{-- Comentario Blade --}}
